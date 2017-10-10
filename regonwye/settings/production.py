b55de79b9bc76ecbb600d28c0795ce814e619a8d
ibrobabs <babskolawole@gmail.com>
2017-10-10 09:56:59 -0600

    update

--- regonwye/settings/production.py
+++ regonwye/settings/production.py
@@ -2,6 +2,11 @@ from __future__ import absolute_import, unicode_literals
 
 from regonwye.aws.conf import *
 from decouple import config
+
+from boto.s3.connection import S3Connection
+
+s3 = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+
 from .base import *
 
 DEBUG = False
