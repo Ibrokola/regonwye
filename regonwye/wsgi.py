96b5f9a66df81818804587d72bb4a546f56d6989
ibrobabs <babskolawole@gmail.com>
2017-10-10 10:47:21 -0600

    update

--- regonwye/wsgi.py
+++ regonwye/wsgi.py
@@ -4,6 +4,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "regonwye.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "regonwye.settings.production")
 
 application = get_wsgi_application()
