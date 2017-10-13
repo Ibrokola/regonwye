7124d216cea7b47c20f55d8b595855db2caaa4cb
ibrobabs <babskolawole@gmail.com>
2017-10-13 15:40:39 -0600

    update

--- blog/templatetags/reg_blogtags.py
+++ blog/templatetags/reg_blogtags.py
@@ -18,12 +18,3 @@ def blog_listing_homepage(context, count=3):
         'request': context['request'],
     }
 
-# # Article feed for home page
-# @register.inclusion_tag('core/tags/homepage_article_listing.html', takes_context=True)
-# def homepage_article_listing(context, count=6):
-#     article_posts = play_filter(ArticlePage.objects.filter(live=True).order_by('-date'), count)
-#     return {
-#         'article_posts': article_posts,
-#         # required by the pageurl tag that we want to use within this template
-#         'request': context['request'],
-#     }
