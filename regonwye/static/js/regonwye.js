1a7c136b70091ee78c0d0616596f714b312c2867
ibrobabs <babskolawole@gmail.com>
2017-10-14 14:18:05 -0600

    update

---
+++ regonwye/static/js/regonwye.js
@@ -0,0 +1,19 @@
+var myheader = document.querySelector("h1")
+
+function getRandomColor(){
+    var letters = "0123456789ABCDEF";
+    var color = '#';
+    for(var i=0; i < 6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color
+}
+
+function changeHeaderColor(){
+    colorInput = getRandomColor()
+    myheader.style.color = colorInput;
+}
+
+setInterval("changeHeaderColor()",500);
+
+// var p = document.querySelector('p')
--- services/models.py
+++ services/models.py
@@ -1,3 +1,38 @@
+from __future__ import absolute_import, unicode_literals
+from datetime import date
+from django import forms
+
 from django.db import models
 
-# Create your models here.
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsnippets.models import register_snippet
+from wagtail.wagtailsearch import index
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+
+
+class ServiceIndexPage(Page):
+    pass
+
+
+
+
+
+@register_snippet
+class ServiceCategory(models.Model):
+    pass
+
+
+
+
+class ServiceCategoryPage(models.Model):
+    pass
+
+
+
+class ServicePage(Page):
+    pass
