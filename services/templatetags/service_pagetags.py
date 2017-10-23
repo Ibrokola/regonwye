81f06b00a6adb3caae3268355ed46f0a4d30cd6d
ibrobabs <babskolawole@gmail.com>
2017-10-22 18:55:26 -0600

    update

--- services/templatetags/service_pagetags.py
+++ services/templatetags/service_pagetags.py
@@ -11,7 +11,7 @@ register = template.Library()
 # Blog feed for home page
 @register.inclusion_tag('services/tags/service_listing_homepage.html',takes_context=True)
 def service_listing_homepage(context, count=4):
-    posts = ServicePage.objects.live().order_by('-first_published_at')
+    posts = ServicePage.objects.live().order_by('first_published_at')
     return {
         'posts': posts[:count],
         # required by the pageurl tag that we want to use within this template
