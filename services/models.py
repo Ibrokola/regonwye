36e9b84b7d342a219f24a5db4d03e37dd9884340
ibrobabs <babskolawole@gmail.com>
2017-10-22 18:38:47 -0600

    update

--- services/models.py
+++ services/models.py
@@ -280,6 +280,7 @@ class ServicePage(Page):
         return context
 
     class Meta:
+        ordering = ['date']
         verbose_name = _('Service page')
         verbose_name_plural = _('Services pages')
 
